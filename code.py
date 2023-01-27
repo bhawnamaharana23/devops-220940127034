284f6f94f59cbaaa9065ba9fe220ca89da368210
root <bhawnamaharana23@gmail.com>
2023-01-27 20:08:55 +0530

    First commit

---
+++ code.py
@@ -0,0 +1,8 @@
+a = input("Enter the first number : ")
+b = input("Enter the second number : ")
+print(f"Before swapped the number, a is {a} and b is {b}")
+c = a
+a = b
+b = c
+print(f"After swapped the number, a is {a} and b is {b}")
+
